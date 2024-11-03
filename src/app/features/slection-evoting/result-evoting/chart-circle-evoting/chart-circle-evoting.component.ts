5e744eb48701722662576cb9aaf106c53f711f26
NguyenThuong02 <nguyenthanhthuong02@gmail.com>
2024-11-03 22:16:55 +0700

    style: chart columns result evoting

---
+++ src/app/features/slection-evoting/result-evoting/chart-circle-evoting/chart-circle-evoting.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-circle-evoting',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart-circle-evoting.component.html',
+  styleUrl: './chart-circle-evoting.component.scss'
+})
+export class ChartCircleEvotingComponent {
+
+}
--- src/app/features/slection-evoting/result-evoting/result-evoting.component.ts
+++ src/app/features/slection-evoting/result-evoting/result-evoting.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { ChartEvotingComponent } from './chart-evoting/chart-evoting.component';
+import { ChartCircleEvotingComponent } from './chart-circle-evoting/chart-circle-evoting.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-result-evoting',
   standalone: true,
   imports: [
-    ChartEvotingComponent
+    CommonModule,
+    ChartEvotingComponent,
+    ChartCircleEvotingComponent
   ],
   templateUrl: './result-evoting.component.html',
   styleUrl: './result-evoting.component.scss'
 })
 export class ResultEvotingComponent {
+  public chartType: any = 'columns'
 
+  handleChangeChart(name: string) {
+    this.chartType = name;
+  }
 }

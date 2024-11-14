e69f4e4a9fc05d4ecf3aaa34cd1149864814ca6b
NguyenThuong02 <nguyenthanhthuong02@gmail.com>
2024-11-14 11:20:57 +0700

    feat: view info CTV

--- src/app/features/slection-management/slection-management-list/detail-ctv/detail-ctv.component.ts
+++ src/app/features/slection-management/slection-management-list/detail-ctv/detail-ctv.component.ts
@@ -12,14 +12,13 @@ import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzModalComponent, NzModalModule, NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
 import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { ManagermentService } from '../../../../core/api/managerment.service';
 
 @Component({
   selector: 'app-detail-ctv',
   standalone: true,
   imports: [
     FormsModule,
-    MatInput,
-    MatLabel,
     CommonModule,
     NzModalComponent,
     NzModalModule,
@@ -42,37 +41,39 @@ export class DetailCtvComponent implements OnInit, OnChanges{
   @Input() mode: 'create' | 'edit';
   @Output() visiblePopUpDetail = new EventEmitter<boolean>();
   public edit: boolean = false;
-  public listCandidate: any = [];
-  public listVoter: any = [];
+  public viewInfo: any = {};
 
   constructor(
     private fb: FormBuilder,
     private cdr: ChangeDetectorRef,
     private modal: NzModalService,
     private message: NzMessageService,
+    private managermentService: ManagermentService
   ) {}
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['idCtv']) {
-      if(this.idCtv && this.mode === 'edit') {
-        this.edit = true;
-      } else {
-        this.edit = false;
+      if(this.idCtv) {
+        this.handleView();
       }
     }
   }
   ngOnInit(): void {
-    if(this.idCtv && this.mode === 'edit') {
-      this.edit = true;
-    } else {
-      this.edit = false; 
+    if(this.idCtv) {
+      this.handleView();
     }
   }
 
-  handleOk(): void {
-
+  handleView(): void {
+    this.managermentService.getUserById(this.idCtv).subscribe({
+      next: (res) => {
+        this.viewInfo = res;
+        this.message.success('Lấy thông tin thành công');
+      },
+      error: (err) => {
+        this.message.error('Lỗi lấy thông tin');
+      }
+    })
   }
-
-  handleEdit(): void {}
 
   handleCancel(): void {
     this.visiblePopUpDetail.emit(false);

180b30ef4022271a709cc1824ba0e4a970fb7c6c
NguyenThuong02 <nguyenthanhthuong02@gmail.com>
2024-11-22 14:37:12 +0700

    feat: result evoting

--- src/app/features/slection-evoting/result-evoting/result-evoting.component.ts
+++ src/app/features/slection-evoting/result-evoting/result-evoting.component.ts
@@ -1,22 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ChartEvotingComponent } from './chart-evoting/chart-evoting.component';
 import { ChartCircleEvotingComponent } from './chart-circle-evoting/chart-circle-evoting.component';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ShareTableModule } from '../../../shared/components/share-table/share-table.module';
-import { RouterLink, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterModule } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { TranslateModule } from '@ngx-translate/core';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
 import { PagiComponent } from '../../../shared/components/pagi/pagi.component';
+import { VoteService } from '../../../core/api/vote.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-result-evoting',
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink,
     NzSpinModule,
     FormsModule,
     ReactiveFormsModule,
@@ -29,10 +30,12 @@ import { PagiComponent } from '../../../shared/components/pagi/pagi.component';
   templateUrl: './result-evoting.component.html',
   styleUrl: './result-evoting.component.scss'
 })
-export class ResultEvotingComponent {
+export class ResultEvotingComponent implements OnInit {
   public chartType: any = 'columns'
   public type: any = 'candidate'
   public isLoading: boolean = false;
+  public idVote: any = '';
+  public infoVote: any = {};
   public totalCount: number = 10;
   maxheight: string = '';
   public param = {
@@ -55,6 +58,30 @@ export class ResultEvotingComponent {
       email: '',
     }
   ];
+
+  constructor(
+    private fb: FormBuilder,
+    private cdr: ChangeDetectorRef,
+    private route: ActivatedRoute,
+    private router: Router,
+    private voteService: VoteService,
+    private message: NzMessageService,
+  ){}
+  ngOnInit(): void {
+    this.idVote = this.route.snapshot.paramMap.get('id');
+    this.viewDetailVote(this.idVote);
+  }
+
+  viewDetailVote(id: any) {
+    this.voteService.detailVote(id).subscribe({
+      next: (res) => {
+        this.infoVote = res.data
+      },
+      error: (err) => {
+        this.message.error('Có lỗi xảy ra, vui lòng thử lại sau!');
+      }
+    })
+  }
 
   viewInformation() {
     this.isLoading = true;
